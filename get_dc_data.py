bcb1c2bc332d70c3c0f880a3c65fde5a6c3830cd
Javi <javier.dominguez.jdr@gmail.com>
2021-04-17 09:26:01 +0100

    Initial commit

---
+++ get_dc_data.py
@@ -0,0 +1,66 @@
+import json
+import urllib
+
+
+def get_identifiers(identifiers):
+    info = {}
+    package = identifiers['packages'][0]['id'].split('/')
+    info['package'] = package[0].split(':')[1]
+    if info['package'] == 'maven':
+        dependency = package[1] + '.' + package[2]
+        info['dependency'] = dependency
+        info['project'] = package[1]
+        info['application'], info['version'] = package[2].rsplit('@', 1)
+    elif info['package'] == 'javascript':
+        info['project'] = '-'
+        info['application'], info['version'] = package[1].rsplit('@', 1)
+    elif info['package'] == 'npm':
+        package[1] = urllib.parse.unquote(package[1])
+        info['project'] = '-'
+        try:
+            info['application'], info['version'] = package[1].rsplit('@', 1)
+        except ValueError:
+            info['application'] = '-'
+            info['version'] = '-'
+    else:
+        info['project'] = package[1]
+
+    return info
+
+
+def get_vulnerabilities_data(vulnerabilities):
+    info = []
+    cve_list = []
+    for vulnerability in vulnerabilities:
+        vuln_info = {}
+        vuln_info['CVE'] = vulnerability['name']
+        if vulnerability['name'] not in cve_list:
+            cve_list.append(vulnerability['name'])
+        try:
+            vuln_info['CVSSv3'] = float(vulnerability['cvssv3']['baseScore'])
+        except KeyError:
+            vuln_info['CVSSv3'] = 0.0
+        info.append(vuln_info.copy())
+    return info, cve_list
+
+
+def get_dc_data(dependencies, project):
+    dependency_list = []
+    vuln_list = []
+    for dependency in dependencies:
+        dependency_data = get_identifiers(dependency)
+        dependency_data['project'] = [project]
+        dependency_data['vulnerabilities'] = []
+        if 'vulnerabilities' in dependency:
+            vuln_data, cves = get_vulnerabilities_data(dependency['vulnerabilities'])
+            vuln_list = vuln_list + vuln_data
+            dependency_data['vulnerabilities'] = cves
+        if dependency_data.copy() not in dependency_list:
+            dependency_list.append(dependency_data.copy())
+    return dependency_list, vuln_list
+
+
+def get_depcheck_data(project, file='dependency-check-report.json'):
+    with open(file) as f:
+        dependencies = json.loads(f.read()).get('dependencies')
+    return get_dc_data(dependencies, project)
---
+++ ingest_data_neo4j.py
@@ -0,0 +1,77 @@
+from neo4j import GraphDatabase
+import get_dc_data
+import os
+
+
+def ingest_project(project):
+    tx.run('''
+    MERGE (n:project {project_name: $project})
+    ''', project=project)
+
+
+def ingest_vulns(vulns_list):
+    tx.run('''    UNWIND $mapEntry AS mItem
+              CALL apoc.merge.node(["vulnerability"],
+              {vulnerability_name:mItem["CVE"],
+              severity:mItem["CVSSv3"]})
+              YIELD node
+              return node
+            '''
+           , mapEntry=vulns_list)
+
+
+def ingest_dependencies(dependencies, project):
+    for dependency in dependencies:
+        r = tx.run('''
+          MATCH (d:dependency {dependency: $dependency})
+          return d
+                 ''', dependency=dependency.get('dependency'))
+        if r.single():
+            tx.run('''
+          MATCH (d:dependency {dependency: $dependency})
+          WHERE NOT ($projects  IN d.projects)
+          SET d.projects = d.projects + $projects
+                 ''',
+                   dependency=dependency.get('dependency'), projects=project)
+
+            tx.run('''
+          MATCH (d:dependency {dependency: $dependency})
+          SET d.vulnerabilities = $vulnerabilities
+          ''',
+                   dependency=dependency.get('dependency'), vulnerabilities=dependency.get('vulnerabilities'))
+
+        else:
+            tx.run('''
+            MERGE (d:dependency {package: $package, dependency: $dependency,
+            vulnerabilities: $vulnerabilities, projects: $projects})
+            ''', package=dependency.get('package'), dependency=dependency.get('dependency'),
+                   vulnerabilities=dependency.get('vulnerabilities'), projects=dependency.get('project'))
+
+
+def create_vuln_relations():
+    tx.run('''   MATCH (d:dependency), (v:vulnerability)
+    WHERE v.vulnerability_name IN  d.vulnerabilities
+
+    MERGE(d)-[:VULNERABLE_TO]->(v)
+    ''')
+
+
+def create_project_relations():
+    r = tx.run('''
+    MATCH (d:dependency), (p:project)
+    WHERE p.project_name IN  d.projects
+    MERGE (p)-[:USES]->(d)
+    ''')
+
+
+project = 'testjavi'
+deps, vulns = get_dc_data.get_depcheck_data(project=project)
+driver = GraphDatabase.driver(os.environ.get('NEO4J_DB'),
+                              auth=(os.environ.get('NEO4J_USER'), os.environ.get('NEO4J_PWD')))
+tx = driver.session()
+ingest_project(project)
+ingest_dependencies(deps, project)
+ingest_vulns(vulns)
+create_vuln_relations()
+create_project_relations()
+driver.close()
